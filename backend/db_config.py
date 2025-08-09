3453453adafe7278d77efe56cc32137ea69e99ab
Asik Mohamed <asikasat777@gmail.com>
2025-08-09 10:50:39 +0530

    Update code and fix bugs

--- backend/db_config.py
+++ backend/db_config.py
@@ -3,11 +3,11 @@ import mysql.connector
 def get_db_connection():
     try:
         conn = mysql.connector.connect(
-            host='localhost',
-            port=3307,  # use 3306 if default
-            user='root',
-            password='',
-            database='mutton_order_db'
+            host='bfpp9qnobskouzfj8dds-mysql.services.clever-cloud.com',
+            port=3306,
+            user='ul68ggb8h5wz47pc',
+            password='OfYOpGpNws91i49FL7Nr',
+            database='bfpp9qnobskouzfj8dds'
         )
         print("✅ Database connection successful!")
         return conn
